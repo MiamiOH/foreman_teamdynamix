4cb41889ee88c9eb0f50407b17a3d9f2dea03767
Nipendar Tyagi <nippu.tyagi@gmail.com>
2017-12-17 04:04:54 -0600

    setup basic test to expect teamdynamix tab and pane expect configured through plugin settings

--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -1,6 +1,9 @@
 # This calls the main test_helper in Foreman-core
-require 'test_helper'
+SETTINGS[:team_dynamix] = { appID: 'testAppID',
+                            asset_attributes: {} }
+require 'fake_td_api'
 
+require 'test_helper'
 # Add plugin to FactoryBot's paths
 FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
 FactoryBot.reload
