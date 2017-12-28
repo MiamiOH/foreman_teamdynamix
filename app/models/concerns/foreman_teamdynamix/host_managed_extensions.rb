c374b830e1eaafd15696342b3e0b38a6d81dba00
Nipendar Tyagi <nippu.tyagi@gmail.com>
2017-12-27 23:57:14 -0600

    use setter DI for TD Api object for Host model

--- app/models/concerns/foreman_teamdynamix/host_managed_extensions.rb
+++ app/models/concerns/foreman_teamdynamix/host_managed_extensions.rb
@@ -1,13 +1,18 @@
 module ForemanTeamdynamix 
   module HostManagedExtensions 
     extend ActiveSupport::Concern 
+    attr_writer :td_api
+
+    def td_api
+      @td_api || TeamDynamixApi.new
+    end
     
     included do
       after_create :create_td_asset
     end
   
     def create_td_asset
-      TeamDynamixApi.new.create_asset(self)
+      td_api.create_asset(self)
     end
     
   end
