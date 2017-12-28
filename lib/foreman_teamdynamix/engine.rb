4b18d1c72aeb248879fdcb130b879f500a610aff
Nipendar Tyagi <nippu.tyagi@gmail.com>
2017-12-27 21:58:36 -0600

    add a migration to add teamdynamix_asset_id column to managed host

--- lib/foreman_teamdynamix/engine.rb
+++ lib/foreman_teamdynamix/engine.rb
@@ -9,6 +9,12 @@ module ForemanTeamdynamix
     config.autoload_paths += Dir["#{config.root}/app/overrides"]
     config.autoload_paths += Dir["#{config.root}/lib"]
 
+    initializer 'foreman_teamdynamix.load_app_instance_data' do |app|
+      if ForemanTeamdynamix::Engine.paths['db/migrate'].existent
+        app.config.paths['db/migrate'].concat(ForemanTeamdynamix::Engine.paths['db/migrate'].to_a)
+      end
+    end
+
     initializer 'foreman_teamdynamix.register_plugin', :before => :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_teamdynamix do
         requires_foreman '>= 1.7'
