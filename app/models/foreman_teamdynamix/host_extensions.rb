8249373079873884b4d29e11f52d8d72bb3a04c3
wimmeldj <wimmeldj@miamioh.edu>
2018-05-03 12:58:58 -0400

    Cleans up host_extensions.rb

--- app/models/foreman_teamdynamix/host_extensions.rb
+++ app/models/foreman_teamdynamix/host_extensions.rb
@@ -8,29 +8,11 @@ module ForemanTeamdynamix
       validates :teamdynamix_asset_uid, uniqueness: { :allow_blank => true }
     end
 
-    private
-
     def td_api
       @td_api ||= TeamdynamixApi.instance
     end
 
-    def create_teamdynamix_asset
-      # when the asset is already in teamdynamix
-      assets = td_api.search_asset(SerialLike: name)
-
-      if assets.empty?
-        asset = td_api.create_asset(self)
-        self.teamdynamix_asset_uid = asset['ID']
-      elsif assets.length > 1
-        raise 'Found more than 1 existing asset'
-      else
-        self.teamdynamix_asset_uid = assets.first['ID']
-        td_api.update_asset(self)
-      end
-    rescue StandardError => e
-      errors.add(:base, _("Could not create the asset for the host in TeamDynamix: #{e.message}"))
-      false
-    end
+    private
 
     def retire_teamdynamix_asset
       td_api.retire_asset(teamdynamix_asset_uid) if teamdynamix_asset_uid
